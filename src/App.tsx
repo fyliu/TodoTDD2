bba030760034de751a6dd279eed667f81c8570a7
fyliu <fyliu@users.noreply.github.com>
2022-01-26 22:56:20 -0800

    Implement Step 15

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 import React, { useState } from "react";
 import { AddTaskModal } from "./components/AddTaskModal";
+import { TaskList } from "./components/TaskList";
 import { TaskItem } from "./components/TaskListItem";
 
 export default function App() {
   const [showAddTaskModal, setShowAddTaskModal] = useState(false);
+  const [tasks, setTasks] = useState<TaskItem[]>([]);
 
   const addTaskHandler = (taskItem: TaskItem) => {
     setShowAddTaskModal(false);
+    setTasks((prev) => [...prev, taskItem]);
   };
 
   return (
@@ -23,6 +26,7 @@ export default function App() {
           Add Task
         </button>
       )}
+      <TaskList tasks={tasks} />
     </>
   );
 }
