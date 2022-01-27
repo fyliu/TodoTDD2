586fff5091798ddd977f174d342c81414741c7cf
fyliu <fyliu@users.noreply.github.com>
2022-01-26 21:19:40 -0800

    Fix App import

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,6 +1,6 @@
 import * as React from "react";
 import { render, screen, waitFor } from "@testing-library/react";
-import App from "../App";
+import App from "./App";
 import userEvent from "@testing-library/user-event";
 
 describe("App", () => {
