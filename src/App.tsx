ef1cccc6473b18c6cbb1684528a69ac9123d8b56
fyliu <fyliu@users.noreply.github.com>
2022-01-26 22:20:38 -0800

    Implement Step 12

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
+import { AddTaskModal } from "./components/AddTaskModal";
 import { TaskItem } from "./components/TaskListItem";
 
 export default function App() {
+  const [showAddTaskModal, setShowAddTaskModal] = useState(false);
+
   const addTaskHandler = (taskItem: TaskItem) => {};
 
-  return <>Task Tracker</>;
+  return (
+    <>
+      Task Tracker
+      {showAddTaskModal ? (
+        <AddTaskModal />
+      ) : (
+        <button
+          onClick={() => {
+            setShowAddTaskModal(true);
+          }}
+        >
+          Add Task
+        </button>
+      )}
+    </>
+  );
 }
